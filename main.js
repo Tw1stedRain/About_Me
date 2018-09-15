74cba721b0ebe5f0930e40ec3ae6224e01017dac
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2018-09-15 13:28:19 -0700

    edited CSS, converted questions into functions

--- main.js
+++ main.js
@@ -1,121 +1,167 @@
 'use strict';
+
+//global variables
+var userName;
+
 //to gain userName
-var userName = prompt ('Welcome to my site, what\'s your name?');
-console.log('What is your name? : ' + userName);
+var useername = function(){
+
+  var userName = prompt ('Welcome to my site, what\'s your name?');
+  console.log('What is your name? : ' + userName);
+};
 
 //coding question
-var coding = prompt ('Does Nicole like coding?');
-coding = coding.toLowerCase();
 
-if (coding === 'yes' || coding === 'y'){
-  alert ('Great job, ' + userName + ' of course I love coding!');
-} else if(coding === 'no' || coding === 'n'){
-  alert( 'Oh No! Of course I love coding!');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
-console.log('Does Nicole like coding? : ' + coding);
+var codingQuestion = function(){
+
+  var coding = prompt ('Does Nicole like coding?');
+  coding = coding.toLowerCase();
+
+  if (coding === 'yes' || coding === 'y'){
+    alert ('Great job, ' + userName + ' of course I love coding!');
+  } else if(coding === 'no' || coding === 'n'){
+    alert( 'Oh No! Of course I love coding!');
+  } else {
+    alert('Please respond using yes or no or y or n.');
+  }
+  console.log('Does Nicole like coding? : ' + coding);
+};
 
 // anime question
-var anime = prompt ('Does Nicole like anime?');
-anime = anime.toLowerCase();
 
-if (anime === 'yes' || anime === 'y'){
-  alert ('Great job, ' + userName + ' anime is great right?!');
-} else if(anime === 'no' || anime === 'n'){
-  alert( 'Oh No! Of course I love anime!');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
+var animeQuestion = function(){
 
-console.log('Does Nicole like anime? : ' + anime);
+  var anime = prompt ('Does Nicole like anime?');
+  anime = anime.toLowerCase();
+
+  if (anime === 'yes' || anime === 'y'){
+    alert ('Great job, ' + userName + ' anime is great right?!');
+  } else if(anime === 'no' || anime === 'n'){
+    alert( 'Oh No! Of course I love anime!');
+  } else {
+    alert('Please respond using yes or no or y or n.');
+  }
+
+  console.log('Does Nicole like anime? : ' + anime);
+};
 
 //country music
-var countryMusic = prompt ('Does Nicole like country music?');
-countryMusic = countryMusic.toLowerCase();
 
-if (countryMusic === 'no' || countryMusic === 'n'){
-  alert ('Great job, ' + userName + ' I really don\'t like country music');
-} else if(countryMusic === 'yes' || countryMusic === 'y'){
-  alert( 'Oh No! I\'m really not a fan of country music.');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
+var countryMusicQuestion = function(){
 
-console.log('Does Nicole like country music? : ' + countryMusic);
+  var countryMusic = prompt ('Does Nicole like country music?');
+  countryMusic = countryMusic.toLowerCase();
+
+  if (countryMusic === 'no' || countryMusic === 'n'){
+    alert ('Great job, ' + userName + ' I really don\'t like country music');
+  } else if(countryMusic === 'yes' || countryMusic === 'y'){
+    alert( 'Oh No! I\'m really not a fan of country music.');
+  } else {
+    alert('Please respond using yes or no or y or n.');
+  }
+
+  console.log('Does Nicole like country music? : ' + countryMusic);
+};
 
 //single question
-var single = prompt ('Is Nicole single?');
-single = single.toLowerCase();
 
-if (single === 'yes' || single === 'y'){
-  alert ('That\'s right, ' + userName + ' I am single.');
-} else if(single === 'no' || single === 'n'){
-  alert( 'Oh No! I\'m actually single.');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
+var singleQuestion = function(){
 
-console.log('Is Nicole single? : ' + single);
+  var single = prompt ('Is Nicole single?');
+  single = single.toLowerCase();
+
+  if (single === 'yes' || single === 'y'){
+    alert ('That\'s right, ' + userName + ' I am single.');
+  } else if(single === 'no' || single === 'n'){
+    alert( 'Oh No! I\'m actually single.');
+  } else {
+    alert('Please respond using yes or no or y or n.');
+  }
+
+  console.log('Is Nicole single? : ' + single);
+};
 
 //turtles!
-var turtle = prompt ('Does Nicole like turtles?');
-turtle = turtle.toLowerCase();
 
-if (turtle === 'yes' || turtle === 'y'){
-  alert ('That\'s right, ' + userName + ' I LOVE turtles!');
-} else if(turtle === 'no' || turtle === 'n'){
-  alert( 'Oh No! I LOVE turtles!');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
+var turtleQuestion = function(){
 
-console.log('Does Nicole like turtles? : ' + turtle);
+  var turtle = prompt ('Does Nicole like turtles?');
+  turtle = turtle.toLowerCase();
+
+  if (turtle === 'yes' || turtle === 'y'){
+    alert ('That\'s right, ' + userName + ' I LOVE turtles!');
+  } else if(turtle === 'no' || turtle === 'n'){
+    alert( 'Oh No! I LOVE turtles!');
+  } else {
+    alert('Please respond using yes or no or y or n.');
+  }
+
+  console.log('Does Nicole like turtles? : ' + turtle);
+};
 
 //number question
-alert('Now ' + userName + ', for this next question, please answer using a number:');
 
-var attempts = 4;
+var stepsQuestion = function(){
 
-while (attempts > 0 ){
-  var steps = prompt('At least how many steps did I take today?');
-  if (steps >= 10000){
-    attempts = 0;
-    console.log('SUCCESS<<<<', steps, attempts);
-    alert ('That\'s right, that\'s exactly how many steps I took today');
-    break;
-  } else if( steps <= 500){
-    alert ('Nope, that\'s waayy too low, try again ' + userName);
-  } else if (500 < steps < 9999){
-    alert ('Too low, try again?');
-  } else if (steps < 0){
-    alert( 'I didn\'t know I could return steps! How is that possible? Nevermind, don\'t answer that.');
-  } else if(typeof steps !== 'number'){
-    alert('it has to be a number!!!');
-    console.log('fail<<<<', steps, attempts);
+  alert('Now ' + userName + ', for this next question, please answer using a number:');
+
+  var attempts = 4;
+
+  while (attempts > 0 ){
+    var steps = prompt('At least how many steps did I take today?');
+    if (steps >= 10000){
+      attempts = 0;
+      console.log('SUCCESS<<<<', steps, attempts);
+      alert ('That\'s right, that\'s exactly how many steps I took today');
+      break;
+    } else if( steps <= 500){
+      alert ('Nope, that\'s waayy too low, try again ' + userName);
+    } else if (500 < steps < 9999){
+      alert ('Too low, try again?');
+    } else if (steps < 0){
+      alert( 'I didn\'t know I could return steps! How is that possible? Nevermind, don\'t answer that.');
+    } else if(typeof steps !== 'number'){
+      alert('it has to be a number!!!');
+      console.log('fail<<<<', steps, attempts);
+    }
+    attempts--;
+
   }
-  attempts--;
-
-}
-console.log('How many steps did I take today? : ' + steps);
+  console.log('How many steps did I take today? : ' + steps);
+};
 
 //multiple choice question
-var hairColors = ['blonde', 'black', 'blue', 'ginger', 'red', 'purple', 'green', 'silver', 'brown'];
 
-var tries = 6;
+var hairQuestion = function(){
 
-while (tries > 0 ){
-  var hair = prompt('What is one of the different colors that I\'ve dyed my hair?');
-  hair = hair.toLowerCase();
-  if (hairColors.includes(hair)){
-    tries = 0;
-    console.log('SUCCESS<<<<', hair, tries);
-    alert ('Corect response! Good job!');
-    break;
-  } else{
-    alert ('Nope, maybe I should try that next. Try again.');
-    console.log('fail<<<<', hair, tries);
+  var hairColors = ['blonde', 'black', 'blue', 'ginger', 'red', 'purple', 'green', 'silver', 'brown'];
+
+  var tries = 6;
+
+  while (tries > 0 ){
+    var hair = prompt('What is one of the different colors that I\'ve dyed my hair?');
+    hair = hair.toLowerCase();
+    if (hairColors.includes(hair)){
+      tries = 0;
+      console.log('SUCCESS<<<<', hair, tries);
+      alert ('Corect response! Good job!');
+      break;
+    } else{
+      alert ('Nope, maybe I should try that next. Try again.');
+      console.log('fail<<<<', hair, tries);
+    }
+    tries--;
   }
-  tries--;
-}
-console.log('What is one of the colors I\'ve dyed my hair? : ' + hair);
+  console.log('What is one of the colors I\'ve dyed my hair? : ' + hair);
+};
+
+// calling the functions
+
+useername();
+codingQuestion();
+animeQuestion();
+countryMusicQuestion();
+singleQuestion();
+turtleQuestion();
+stepsQuestion();
+hairQuestion();
